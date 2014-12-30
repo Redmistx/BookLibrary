721a1f0e62f151dfe1568e51ca060888e20b962c
Angel Vasilev <enzeru@mail.bg>
2014-12-31 00:34:58 +0200

    Added ViewModels folder and BookViewModel for the basic displaying of the books

---
+++ Source/Web/BookLibrary.Web/ViewModels/Home/BookViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BookLibrary.Data.Models;
+using BookLibrary.Web.Infrastructure.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookLibrary.Web.ViewModels.Home
+{
+    public class BookViewModel : IMapFrom<Book>
+    {
+        public string Name { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public string RentedBy { get; set; }
+
+        public string LastRenter { get; set; }
+    }
+}
